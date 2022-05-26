09df1e8b2c85fcf531871e1aabba9f5e95991be5
Thiago Aragao <thiagohoaragao@gmail.com>
2022-05-26 15:36:05 -0300

    Made back-end

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :lists, except: %i[update destroy edit] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: %i[destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'lists#index'
 end
